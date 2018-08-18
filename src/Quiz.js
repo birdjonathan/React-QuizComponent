c841b751c461d2afd12943bb0fece73b642b01d7
Jonathan Bird <jonathanbird@gmail.com>
2018-08-18 12:45:08 -0400

    Finishes module 6

--- src/Quiz.js
+++ src/Quiz.js
@@ -4,13 +4,23 @@ import QuizEnd from './QuizEnd'
 
 let quizData = require('./quiz_data.json');
 
+
 class Quiz extends Component {
     constructor(props) {
         super(props);
         this.state = { quiz_position: 1 };
-
+        
     }
+    
+    showNextQuestion () {
+        this.setState((state) => {
+            return { quiz_position: state.quiz_position + 1} 
+        })
+            
+    }
+    
     render() {
+
         const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length 
         
         if (isQuizEnd) {
@@ -22,7 +32,7 @@ class Quiz extends Component {
         } else {
             return (
                 <div>
-                    <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
+                    <QuizQuestion showNextQuestionHandler={this.showNextQuestion.bind(this)} quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
                 </div>
             )
         }
